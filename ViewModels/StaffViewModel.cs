83168842dd816a5cad0381390a97044c64754260
stevensmithmsc <stevensmithmsc@gmail.com>
2016-06-17 15:10:18 +0100

    Entity Framework Added
    Entity Framework added to project, Staff Tables modelled and Staff
    ViewModel started.

---
+++ ViewModels/StaffViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFParisTraining.Entity;
+
+namespace WPFParisTraining.ViewModels
+{
+    class StaffViewModel : ViewModel
+    {
+        private StaffEntities db;
+
+        private ObservableCollection<Staff> _staffList;
+        public ObservableCollection<Staff> StaffList { get { return _staffList; } set { _staffList = value;  NotifyPropertyChanged(); } }
+
+        private Staff _selectedStaff;
+        public Staff SelectedStaff { get { return _selectedStaff; } set { _selectedStaff = value;  NotifyPropertyChanged(); } }
+
+        public List<Title> Titles { get; private set; } 
+
+        public StaffViewModel()
+        {
+            db = new StaffEntities();
+            db.Staffs.Load();
+            StaffList = db.Staffs.Local;
+            SelectedStaff = StaffList.First();
+            db.Titles.Load();
+            Titles = db.Titles.Local.ToList();
+            NotifyPropertyChanged("Titles");
+        }
+    }
+}
