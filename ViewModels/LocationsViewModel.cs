39183b74df4586e38c738ebb5fc459ba15c3a23d
steven.smith <steven.smith@PEN-20938-LAP>
2016-06-21 16:40:21 +0100

    Team Approvals section added
    Team Approvals section added to Staff View

---
+++ ViewModels/LocationsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFParisTraining.Entity;
+
+namespace WPFParisTraining.ViewModels
+{
+    class LocationsViewModel : ViewModel
+    {
+        private StaffEntities db;
+
+        
+
+        public LocationsViewModel()
+        {
+            db = new StaffEntities();
+
+        }
+    }
+}
