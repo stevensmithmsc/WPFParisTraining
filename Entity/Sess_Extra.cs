ceddde215cef7676ba0fd5fd6cec7f941e094d36
steven.smith <steven.smith@PEN-20938-LAP>
2016-06-28 16:06:23 +0100

    Amended Session Start Cell Template

---
+++ Entity/Sess_Extra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFParisTraining.Entity
+{
+    public partial class Sess
+    {
+        public Nullable<DateTime> StartDate { get { if (Strt == null) return null; else return ((DateTime)Strt).Date; }
+                                              set { if (value != null) { if (Strt == null) Strt = value; else { Strt = ((DateTime)value).Date.Add(((DateTime)Strt).TimeOfDay); } } } }
+        public Nullable<TimeSpan> StartTime { get { if (Strt == null) return null; else return ((DateTime)Strt).TimeOfDay; }
+                                              set { if (value != null) { if (Strt == null) Strt = DateTime.Now.Date.Add((TimeSpan)value); else { Strt = ((DateTime)Strt).Date.Add((TimeSpan)value); } } } }
+    }
+}
