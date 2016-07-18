4475137c7dacae0e7a38b5cdd532c8c37cef538a
steven.smith <steven.smith@PEN-20938-LAP>
2016-07-18 16:51:00 +0100

    Converters BooltoFontWeight and BooltoFontStyle created
    Converts created to convert boolean to Bold when true and boolean to
    italic when false.

---
+++ Helpers/BooltoFontWeight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFParisTraining.Helpers
+{
+    class BooltoFontWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            FontWeight returnValue = FontWeights.Normal;
+            if (value is bool)
+            {
+                returnValue = (bool)value ? FontWeights.Bold : FontWeights.Normal;
+            }
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
